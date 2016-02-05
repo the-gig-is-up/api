a53f4d4e671a0b2fc8400cda0921e9fb41d93feb
Gurpreet Paul <gurpreet@gurpreet.co>
2016-02-05 17:48:28 +0000

    Made app work with new StrongLoop

--- server/server.js
+++ server/server.js
@@ -8,9 +8,18 @@ app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
-    console.log('Web server listening at: %s', app.get('url'));
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
   });
 };
+
+app.use('/express-status', function(req, res, next) {
+  res.json({ running: true });
+});
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
